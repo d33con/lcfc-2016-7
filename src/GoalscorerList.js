c029b608958c4398c3445b3f9b8a50cde4993bff
Oli Bullen <d33con@gmail.com>
2017-02-17 16:18:57 +0000

    functional component

--- src/GoalscorerList.js
+++ src/GoalscorerList.js
@@ -1,17 +1,21 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 
 import List from 'react-md/lib/Lists/List';
 import ListItem from 'react-md/lib/Lists/ListItem';
 
-const GoalscorerList = props =>
-  <List className={"md-cell--6 md-text-" + props.textAlign}>
-    {props.scorers.map(scorer => <ListItem key={scorer.time} primaryText={`${scorer.name} ${scorer.time}`} />)}
-  </List>
-
-
-GoalscorerList.propTypes = {
-  scorers: PropTypes.array,
-  textAlign: PropTypes.string
+const GoalscorerListProps = {
+  scorers: React.PropTypes.array.isRequired,
+  textAlign: React.PropTypes.string.isRequired
 };
+
+function GoalscorerList ({scorers, textAlign}) {
+  return (
+    <List className={"md-cell--6 md-text-" + textAlign}>
+      {scorers.map(scorer => <ListItem key={scorer.time} primaryText={`${scorer.name} ${scorer.time}`} />)}
+    </List>
+  )
+}
+
+GoalscorerList.propTypes = GoalscorerListProps;
 
 export default GoalscorerList;
