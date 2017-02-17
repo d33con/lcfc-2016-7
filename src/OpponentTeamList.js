09b3f26c370bd99f81a5bf42ee6be40d095a4d41
Oli Bullen <d33con@gmail.com>
2017-02-17 17:37:58 +0000

    functional component

--- src/OpponentTeamList.js
+++ src/OpponentTeamList.js
@@ -3,20 +3,28 @@ import React, { PropTypes } from 'react';
 import List from 'react-md/lib/Lists/List';
 import ListItem from 'react-md/lib/Lists/ListItem';
 import Avatar from 'react-md/lib/Avatars';
-import Divider from 'react-md/lib/Dividers';
 import FontIcon from 'react-md/lib/FontIcons';
 
-const OpponentTeamList = props =>
-  <List className={"md-cell--6 md-text-" + props.textAlign}>
-    {props.players.map(player => <ListItem key={player.number} primaryText={player.name} secondaryText={player.replaced ? <span><FontIcon>swap_horiz</FontIcon>{player.replaced}</span> : null} leftAvatar={<Avatar suffix={props.avatarColor} icon={player.number} />} />)}
-    <Divider />
-  </List>
-
-
-OpponentTeamList.propTypes = {
+const OpponentTeamListProps = {
   players: PropTypes.array,
   textAlign: PropTypes.string,
   avatarColor: PropTypes.string
 };
+
+function OpponentTeamList ({players, textAlign, avatarColor}) {
+  return (
+    <List className={"md-cell--6 md-text-" + textAlign}>
+      {players.map(player => 
+        <ListItem 
+          key={player.number} 
+          primaryText={player.name} 
+          secondaryText={player.replaced && <span><FontIcon>swap_horiz</FontIcon>{player.replaced}</span>} 
+          leftAvatar={<Avatar suffix={avatarColor} icon={player.number} />} />
+      )}
+    </List>
+  )
+}
+
+OpponentTeamListProps.propTypes = OpponentTeamListProps;
 
 export default OpponentTeamList;
