bcf54f234b7a54f3057e0c539df26aeaa23bed11
Oli Bullen <d33con@gmail.com>
2017-02-17 17:36:21 +0000

    functional component

--- src/LeicesterTeamList.js
+++ src/LeicesterTeamList.js
@@ -1,25 +1,32 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 
 import List from 'react-md/lib/Lists/List';
 import ListItem from 'react-md/lib/Lists/ListItem';
 import Avatar from 'react-md/lib/Avatars';
-import Divider from 'react-md/lib/Dividers';
 
 import squad from './data/squad';
 
 const getLeicesterPlayerName = (squad, number) => squad.find(x => x.number === number).name;
 
-const LeicesterTeamList = props =>
-  <List className={"md-cell--6 md-text-" + props.textAlign}>
-    {props.players.map(number => <ListItem key={number} primaryText={getLeicesterPlayerName(squad, number)} rightAvatar={<Avatar suffix={props.avatarColor} icon={number} />} />)}
-    <Divider />
-  </List>
-
-
-LeicesterTeamList.propTypes = {
-  players: PropTypes.array,
-  textAlign: PropTypes.string,
-  avatarColor: PropTypes.string
+const LeicesterTeamListProps = {
+  players: React.PropTypes.array.isRequired,
+  textAlign: React.PropTypes.string.isRequired,
+  avatarColor: React.PropTypes.string.isRequired
 };
+
+function LeicesterTeamList ({players, textAlign, avatarColor}) {
+  return (
+    <List className={"md-cell--6 md-text-" + textAlign}>
+      {players.map(number => 
+          <ListItem 
+            key={number} 
+            primaryText={getLeicesterPlayerName(squad, number)} 
+            rightAvatar={<Avatar suffix={avatarColor} icon={number} />} />
+        )}
+    </List>
+  )
+}
+
+LeicesterTeamList.propTypes = LeicesterTeamListProps;
 
 export default LeicesterTeamList;
